8fc5a46d50eea1c4d2e3a622a060ceeddf41005d
Eugeles <eufrales@gmail.com>
2026-05-19 10:58:55 -0300

    empezamos con la api kotr, pero no funciona un import de usuario

---
+++ proyecto/.idea/app/ktor-sample/build.gradle.kts
@@ -0,0 +1,31 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(ktorLibs.plugins.ktor)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+group = "com.example"
+version = "1.0.0-SNAPSHOT"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+dependencies {
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.12")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.12")
+    implementation(ktorLibs.server.config.yaml)
+    implementation(ktorLibs.server.core)
+    implementation(ktorLibs.server.netty)
+    implementation(libs.logback.classic)
+
+    testImplementation(kotlin("test"))
+    testImplementation(ktorLibs.server.testHost)
+    implementation("mysql:mysql-connector-java:8.0.33")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+}
+
---
+++ proyecto/.idea/app/ktor-sample/src/main/kotlin/com/example/main.kt
@@ -0,0 +1,8 @@
+package com.example
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
