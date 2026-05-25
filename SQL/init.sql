698533b8878e92d2af5d927d874fee3f2fdead90
eugeama <amaliagillig2508@gmail.com>
2026-05-25 02:41:45 -0300

    se arregló el hardcodeo de mysql

--- SQL/init.sql
+++ SQL/init.sql
@@ -1,10 +1,10 @@
 USE my_database;
 
 CREATE TABLE IF NOT EXISTS usuarios (
-    id      INT          NOT NULL,
+    id      INT          NOT NULL AUTO_INCREMENT,
     nombre  VARCHAR(255) NOT NULL,
     creado  DATE         NOT NULL,
     PRIMARY KEY (id)
 );
 
-INSERT INTO usuarios (id, nombre, creado) VALUES (1, 'Admin', CURDATE());
+INSERT INTO usuarios (nombre, creado) VALUES ('Admin', CURDATE());
